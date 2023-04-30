a2424a5c60e5fec344a93a7940942d36985afb52
jfergu90 <122572328+jfergu90@users.noreply.github.com>
2023-04-30 17:44:41 -0400

    Import/Export
    Updated code to add functionalities to import and export data

--- spendwise.py
+++ spendwise.py
@@ -106,9 +106,7 @@ def savings_calculator(budget):
     print("To reach your financial goal, you need to save $", savings_required, "per month.")
 
 
-# Welcome message
-print("\nManage your expenses easily and save smarter with our intuitive software. Let's get started!")
-print("This app allows you to record and view your spending habits to help you become a more conscious spender!")
+
 
 
 #NEW FEATURE: IMPLEMENT A BUDGETING CATEGORY
@@ -158,13 +156,86 @@ def view_category_budgets():
     for budget in results:
         print(f'{budget[0]}: ${budget[1]}')
 
+import csv
 
+def export_data_to_csv(filename):
+    import csv
+
+    conn = sqlite3.connect("spent.db")
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM expenses")
+    results = cur.fetchall()
+
+    with open(filename, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(["amount", "category", "message", "date"])
+
+        for expense in results:
+            csv_writer.writerow(expense)
+
+    print(f"Data exported to {filename} successfully.")
+
+def import_data_from_csv(filename):
+    import csv
+
+    conn = sqlite3.connect("spent.db")
+    cur = conn.cursor()
+
+    # Read expenses from the CSV file
+    with open(filename, mode='r', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # Skip the header row
+        for row in reader:
+            amount, category, message, date = row
+            sql = '''
+            INSERT INTO expenses (amount, category, message, date)
+            VALUES (?, ?, ?, ?)
+            '''
+            cur.execute(sql, (amount, category, message, date))
+
+    conn.commit()
+    print(f"Data imported from {filename}")
+
+
+    # Read expenses from the CSV file
+    with open(filename, mode='r', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # Skip the header row
+        for row in reader:
+            amount, category, message, date = row
+            sql = '''
+            INSERT INTO expenses (amount, category, message, date)
+            VALUES (?, ?, ?, ?)
+            '''
+            cur.execute(sql, (amount, category, message, date))
+
+    conn.commit()
+    print(f"Data imported from {filename}")
+
+
+    # Fetch all expenses
+    cur.execute("SELECT * FROM expenses")
+    expenses = cur.fetchall()
+
+    # Write expenses to a CSV file
+    with open(filename, mode='w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['amount', 'category', 'message', 'date'])
+        for expense in expenses:
+            writer.writerow(expense)
+
+    print(f"Data exported to {filename}")
+
+# Welcome message
+print("\nManage your expenses easily and save smarter with our intuitive software. Let's get started!")
+print("This app allows you to record and view your spending habits to help you become a more conscious spender!")
 
 #Main loop for user input
 total= input("What is your current total budget?\n:")
 while True:
         print("\nWhat would you like to do?")
-        print("1 - Initialize an expense database (only do this once)\n2 - Enter an expense\n3 - View expenses based on date and category\n4 - Compare Month\n5 - Check Balance\n6 - Update balance\n7 - Calculate Savings Required\n8 - Set Category Budget\n9 - View Category Budgets\nQ - Quit")
+        print("1 - Initialize an expense database (only do this once)\n2 - Enter an expense\n3 - View expenses based on date and category\n4 - Compare Month\n5 - Check Balance\n6 - Update balance\n7 - Calculate Savings Required\n8 - Set Category Budget\n9 - View Category Budgets\n10 - Export Data to CSV\n11 - Import Data from CSV\nQ - Quit")
         ans = input(":")
         print()
 
@@ -195,9 +266,7 @@ while True:
                 total=input("Only in numbers please, \nWhat is your new balance?\n:")
         elif ans == "7":
             savings_calculator(total)
-        elif ans.lower() == "q":
-            print('Goodbye!\n')
-            break
+        
         elif ans == "8":
             cat = input('What is the category for which you want to set a budget?\n1 - Food\n2 - Entertainment\n3 - Education\n4 - Travel\n5 - Car\n6 - Utilities\n7 - Insurance\n8 - Other\n:').title()
             budget = input(f'What is the budget for {cat}?\n:')
@@ -205,6 +274,18 @@ while True:
         elif ans == "9":
             print('\nCategory Budgets:')
             view_category_budgets()
+
+        elif ans == "10":
+            filename = input("Enter the name of the CSV file you want to export data to (e.g., expenses.csv):\n")
+            export_data_to_csv(filename)
+
+        elif ans == "11":
+            filename = input("Enter the name of the CSV file you want to import data from (e.g., expenses.csv):\n")
+            import_data_from_csv(filename)
+
+        elif ans.lower() == "q":
+            print('Goodbye!\n')
+            break
 
 
 
